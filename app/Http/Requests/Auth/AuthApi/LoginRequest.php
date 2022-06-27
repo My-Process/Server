67a07f501886fdb73f91b1a7278757727ac6b0ea
João Pedro Lopes <joaopalopes24@gmail.com>
2022-06-27 00:19:51 -0300

    feat(auth): separating request files from authentication

--- app/Http/Requests/Auth/AuthApi/LoginRequest.php
+++ app/Http/Requests/Auth/AuthApi/LoginRequest.php
@@ -33,7 +33,7 @@ class LoginRequest extends FormRequest
             RateLimiter::hit($this->throttleKey());
 
             throw ValidationException::withMessages([
-                'email' => __('auth.failed'),
+                'email' => trans('auth.failed'),
             ]);
         }
 
