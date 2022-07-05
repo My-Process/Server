78a553e161b6962552a4d57a29e0e0d7e67d3a62
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-04 23:49:21 -0300

    refactor(login): refactoring login and email verification screens

--- resources/views/auth/verify-email.blade.php
+++ resources/views/auth/verify-email.blade.php
@@ -1,24 +1,23 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-            </a>
-        </x-slot>
-
-        <div class="mb-4 text-sm text-gray-600">
-            {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+<x-app-layout>
+    <div class="min-h-full flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+        <div class="sm:mx-auto sm:w-full sm:max-w-md">
+            <h2 class="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                <b class="text-amber-600">{{ __('My') }}</b>&nbsp{{ __('Process') }}
+            </h2>
         </div>
 
-        <div class="mt-4 flex items-center justify-between">
-
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-
-                <button type="submit" class="underline text-sm text-gray-600 hover:text-gray-900">
-                    {{ __('Log Out') }}
-                </button>
-            </form>
+        <div class="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+            <div class="text-center bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
+                <div class="mb-4 text-sm text-gray-600">
+                    {{ __('Thanks for signing up! Before getting started, could you verify your email address by clicking on the link we just emailed to you? If you didn\'t receive the email, we will gladly send you another.') }}
+                </div>
+                <form method="post" action="{{ route('logout') }}">
+                    @csrf
+                    <x-button class="w-full justify-center" type="submit" outline>
+                        {{ __('Log Out') }}
+                    </x-button>
+                </form>
+            </div>
         </div>
-    </x-auth-card>
-</x-guest-layout>
+    </div>
+</x-app-layout>
