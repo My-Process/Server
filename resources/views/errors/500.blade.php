2c5344c83b702b2775e5bb9034856cc83c3d96b7
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-04 23:50:56 -0300

    feat(erros): custom error pages to tailwind

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,26 @@
+<x-app-layout>
+    <div class="min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+        <div class="max-w-max mx-auto">
+            <main class="sm:flex">
+                <p class="text-4xl font-extrabold text-amber-600 sm:text-5xl">
+                    500
+                </p>
+                <div class="sm:ml-6">
+                    <div class="sm:border-l sm:border-gray-700 sm:pl-6">
+                        <h1 class="text-4xl font-extrabold text-gray-900 tracking-tight sm:text-5xl">
+                            {{ __('errors.server-error') }}
+                        </h1>
+                        <p class="mt-1 text-base text-gray-500">
+                            {{ __('errors.server-error-text') }}
+                        </p>
+                    </div>
+                    <div class="mt-10 flex space-x-3 sm:border-l sm:border-transparent sm:pl-6">
+                        <x-button href="{{ route('dashboard') }}" outline>
+                            {{ __('errors.home-page') }}
+                        </x-button>
+                    </div>
+                </div>
+            </main>
+        </div>
+    </div>
+</x-app-layout>
