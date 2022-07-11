2bc7f3eaf1b248aecad2bfe8f73e87d2a04d3e78
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-11 19:20:48 -0300

    feat(lang): adding portuguese language

--- lang/en/errors.php
+++ lang/en/errors.php
@@ -8,27 +8,12 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'unauthorized'      => 'Unauthorized',
-    'unauthorized-text' => 'The client must authenticate itself to get the requested response.',
-
-    'forbidden'      => 'Forbidden',
-    'forbidden-text' => 'The customer does not have access rights to the content.',
-
-    'not-found'      => 'Not Found',
-    'not-found-text' => 'Check the URL in the address bar and try again.',
-
-    'page-expired'      => 'Page Expired',
-    'page-expired-text' => 'The page the user wants to load is expired.',
-
-    'too-many-requests'      => 'Too Many Requests',
-    'too-many-requests-text' => 'The user sent too many requests in a given time interval.',
-
-    'server-error'      => 'Server Error',
-    'server-error-text' => 'The server has encountered a situation it doesn\'t know how to handle.',
-
-    'service-unavailable'      => 'Service Unavailable',
-    'service-unavailable-text' => 'The server is not ready to handle the request.',
-
-    'home-page' => 'Dashboard',
+    'unauthorized'        => 'The user must authenticate itself to get the requested response.',
+    'forbidden'           => 'The user does not have access rights to the content.',
+    'not-found'           => 'Check the URL in the address bar and try again.',
+    'page-expired'        => 'The page the user wants to load is expired.',
+    'too-many-requests'   => 'The user sent too many requests in a given time interval.',
+    'server-error'        => 'The server has encountered a situation it doesn\'t know how to handle.',
+    'service-unavailable' => 'The server is not ready to handle the request.',
 
 ];
--- lang/pt-br/errors.php
+++ lang/pt-br/errors.php
@@ -8,27 +8,12 @@ return [
     |--------------------------------------------------------------------------
     */
 
-    'unauthorized'      => 'Não Autorizado',
-    'unauthorized-text' => 'O usuário deve autenticar-se para obter a resposta solicitada.',
-
-    'forbidden'      => 'Proibido',
-    'forbidden-text' => 'O usuário não tem direitos de acesso ao conteúdo.',
-
-    'not-found'      => 'Não Encontrado',
-    'not-found-text' => 'Verifique o URL na barra de endereço e tente novamente.',
-
-    'page-expired'      => 'Página Expirada',
-    'page-expired-text' => 'A página que o usuário deseja carregar expirou.',
-
-    'too-many-requests'      => 'Muitas Requisições',
-    'too-many-requests-text' => 'O usuário enviou muitas solicitações em um determinado intervalo de tempo.',
-
-    'server-error'      => 'Erro de Servidor',
-    'server-error-text' => 'O servidor encontrou uma situação que não sabe como lidar.',
-
-    'service-unavailable'      => 'Serviço Não Disponível',
-    'service-unavailable-text' => 'O servidor não está pronto para lidar com a solicitação.',
-
-    'home-page' => 'Página Inicial',
+    'unauthorized'        => 'O usuário deve autenticar-se para obter a resposta solicitada.',
+    'forbidden'           => 'O usuário não tem direitos de acesso ao conteúdo.',
+    'not-found'           => 'Verifique o URL na barra de endereço e tente novamente.',
+    'page-expired'        => 'A página que o usuário deseja carregar expirou.',
+    'too-many-requests'   => 'O usuário enviou muitas solicitações em um determinado intervalo de tempo.',
+    'server-error'        => 'O servidor encontrou uma situação que não sabe como lidar.',
+    'service-unavailable' => 'O servidor não está pronto para lidar com a solicitação.',
 
 ];
