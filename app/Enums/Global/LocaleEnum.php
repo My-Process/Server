ce7d1e6cd12cab6d266940b9a4a97f9e43dbdb6c
João Pedro Lopes <joaopalopes24@gmail.com>
2022-07-11 19:30:11 -0300

    feat(lang): adding macro in provider and creating enum to set locations

---
+++ app/Enums/Global/LocaleEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums\Global;
+
+use BenSampo\Enum\Enum;
+
+final class LocaleEnum extends Enum
+{
+    public const English    = 'en';
+    public const Portuguese = 'pt-br';
+}
